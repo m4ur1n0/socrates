9edc315fafd46e622ee64c034b500471668f106d
theo <thjmaurino@gmail.com>
2025-04-29 23:27:02 -0500

    fixed image path

--- src/components/NavBarRight.tsx
+++ src/components/NavBarRight.tsx
@@ -10,7 +10,7 @@ const NavBarRight = () => {
     <div className='h-screen flex flex-col items-center w-[8%] bg-gray-200 shadow-inner p-5'>
 
         <div className='user-profile-picture w-[80%]'>
-            <img src={user?.profilePic || '/images/bank_pfp.jpg'} className='object-fit rounded-full ring ring-gray-400 ring-2 ring-offset-2' />
+            <img src={user?.profilePic || '/images/blank_pfp.jpg'} className='object-fit rounded-full ring ring-gray-400 ring-2 ring-offset-2' />
         </div> 
         
     </div>
